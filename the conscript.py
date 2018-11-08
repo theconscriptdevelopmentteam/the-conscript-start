369c10fd4dc55695e4c1d134007821811bfa3ff7
the conscript development team <44879533+theconscriptdevelopmentteam@users.noreply.github.com>
2018-11-08 20:05:01 +0000

    Add files via upload

---
+++ the conscript.py
@@ -0,0 +1,32 @@
+Python 3.6.7rc2 (v3.6.7rc2:4893861ab5, Oct 13 2018, 16:44:52) [MSC v.1900 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/andre/AppData/Local/Programs/Python/Python36-32/time.py =
+ the sludge squelches underfoot, 
+ the walls of the trenches are low far too low to provide cover when standing up 
+ problem is there are too many people to bend over
+ a single stray bullet could end it all 
+ and in the second world war every bullet is stray
+ ~1 year ago~ 
+ You have passed the medical. What is your last name, soldier? 
+ |q
+q  eh? that was my stepfathers uncles grandpas 
+ third cousins twice removeds seventh and final  middle name 
+ the whistle of an enemy bomb drowns out your 
+  innermost thoughts along with the drone of machine gun fire
+ you brace for impact, only to hear the screams of terror and agony from an adjacent trench
+ what do you : 
+ 1, run and hide 
+ 2, run over the top  and help the wounded 
+ 3,  run to cover and continue firing 
+ 4, run and pray 
+ 5 drop where you are 
+ |3
+you see 3 enemy troops, one dead and lying limp into the trench, one aiming his rifle directly at your head, and one rushing you with a trench mace
+who will you fire on? 
+ 1. the enemy soldier's corpse, 
+ 2.the rifleman 
+ 3. the soldier with the mace 
+ |3
+you line up the shot, pull the trigger and see him fall to the ground gripping his wrist as he bleeds out; the rifleman is killed by your machine gunner before he can pull the trigger
+>>> 
